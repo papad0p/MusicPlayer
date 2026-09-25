c839d9466379cb0e1218da38874897c308e31c7b
papad0p <cg.papadop@gmail.com>
2026-09-25 11:57:20 -0400

    Added info button for better UX

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const indexInfoBtn = document.getElementById('index-info-btn');
+const indexInfoDiv = document.getElementById('index-info');
+indexInfoBtn.addEventListener('click', () => {
+    if (indexInfoDiv.style.display === 'block') {
+        indexInfoDiv.style.display = 'none';
+    } else {
+        indexInfoDiv.style.display = 'block';
+    }
+
+});
+
 document.querySelectorAll('ul li[data-playlist]').forEach((li) => {
   li.addEventListener('click', () => {
     const key = li.dataset.playlist;
